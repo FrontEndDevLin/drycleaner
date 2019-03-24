24eca6feb129a91e52db7635f08cf147312b7546
jiangguilan <1063214197@qq.com>
2019-03-25 00:30:02 +0800

    s

--- frontEnd/src/views/Home.vue
+++ frontEnd/src/views/Home.vue
@@ -42,15 +42,21 @@
 					 unique-opened router v-show="!collapsed">
           <template v-for="(item,index) in $router.options.routes" v-if="!item.hidden">
             <!-- <div v-if="item.staff&&level>0"> -->
-              <el-submenu :index="index+''" v-if="!item.leaf&&!item.staff">
+              <el-submenu :index="index+''" v-if="!item.notice&&!item.leaf&&!item.staff">
                 <template slot="title"><i :class="item.iconCls"></i>{{item.name}}</template>
                 <el-menu-item v-for="child in item.children" :index="child.path" :key="child.path" v-if="!child.hidden">{{child.name}}</el-menu-item>
               </el-submenu>
 
               <el-submenu :index="index+'staff'" v-if="item.staff && level>0">
                 <template slot="title"><i :class="item.iconCls"></i>{{item.name}}</template>
-                <el-menu-item v-for="child in item.children" :index="child.path" :key="child.path" v-if="!child.hidden&&child.id!=9">{{child.name}}</el-menu-item>
-                <el-menu-item v-for="child in item.children" :index="child.path" :key="child.id" v-if="!child.hidden&&child.id!=0&&level==99">{{child.name}}</el-menu-item>
+                <el-menu-item v-for="child in item.children" :index="child.path" :key="child.path" v-if="!child.hidden&&child.id==0">{{child.name}}</el-menu-item>
+                <el-menu-item v-for="child in item.children" :index="child.path" :key="child.id" v-if="!child.hidden&&child.id==9&&level==99">{{child.name}}</el-menu-item>
+              </el-submenu>
+
+              <el-submenu :index="index+'notice'" v-if="item.notice && level>0">
+                <template slot="title"><i :class="item.iconCls"></i>{{item.name}}</template>
+                <el-menu-item v-for="child in item.children" :index="child.path" :key="child.path" v-if="!child.hidden&&child.id==4&&level==9">{{child.name}}</el-menu-item>
+                <el-menu-item v-for="child in item.children" :index="child.path" :key="child.id" v-if="!child.hidden&&child.id!=4&&level==99">{{child.name}}</el-menu-item>
               </el-submenu>
 
               <el-menu-item v-if="!item.shop&&item.leaf&&item.children.length>0" :index="item.children[0].path"><i :class="item.iconCls"></i>{{item.children[0].name}}</el-menu-item>
@@ -62,12 +68,26 @@
 				<!--导航菜单-折叠后-->
 				<ul class="el-menu el-menu-vertical-demo collapsed" v-show="collapsed" ref="menuCollapsed">
 					<li v-for="(item,index) in $router.options.routes" v-if="!item.hidden" class="el-submenu item">
-						<template v-if="!item.leaf&&!item.staff">
+						<template v-if="!item.notice&&!item.leaf&&!item.staff">
 							<div class="el-submenu__title" style="padding-left: 20px;" @mouseover="showMenu(index,true)" @mouseout="showMenu(index,false)">
                 <i :class="item.iconCls"></i>
               </div>
 							<ul class="el-menu submenu" :class="'submenu-hook-'+index" @mouseover="showMenu(index,true)" @mouseout="showMenu(index,false)"> 
 								<li v-for="child in item.children" v-if="!child.hidden" :key="child.path" class="el-menu-item" style="padding-left: 40px;" :class="$route.path==child.path?'is-active':''" @click="$router.push(child.path)">
+                  {{child.name}}
+                </li>
+							</ul>
+						</template>
+
+            <template v-if="item.notice && level>0">
+							<div class="el-submenu__title" style="padding-left: 20px;" @mouseover="showMenu(index,true)" @mouseout="showMenu(index,false)">
+                <i :class="item.iconCls"></i>
+              </div>
+							<ul class="el-menu submenu" :class="'submenu-hook-'+index" @mouseover="showMenu(index,true)" @mouseout="showMenu(index,false)"> 
+								<li v-for="child in item.children" v-if="!child.hidden&&child.id==4&&level==9" :key="child.path" class="el-menu-item" style="padding-left: 40px;" :class="$route.path==child.path?'is-active':''" @click="$router.push(child.path)">
+                  {{child.name}}
+                </li>
+								<li v-for="child in item.children" v-if="!child.hidden&&child.id!=4&&level==99" :key="child.id" class="el-menu-item" style="padding-left: 40px;" :class="$route.path==child.path?'is-active':''" @click="$router.push(child.path)">
                   {{child.name}}
                 </li>
 							</ul>
@@ -78,16 +98,22 @@
                 <i :class="item.iconCls"></i>
               </div>
 							<ul class="el-menu submenu" :class="'submenu-hook-'+index" @mouseover="showMenu(index,true)" @mouseout="showMenu(index,false)"> 
-								<li v-for="child in item.children" v-if="!child.hidden&&child.id!=9" :key="child.path" class="el-menu-item" style="padding-left: 40px;" :class="$route.path==child.path?'is-active':''" @click="$router.push(child.path)">
+								<li v-for="child in item.children" v-if="!child.hidden&&child.id==0" :key="child.path" class="el-menu-item" style="padding-left: 40px;" :class="$route.path==child.path?'is-active':''" @click="$router.push(child.path)">
                   {{child.name}}
                 </li>
-								<li v-for="child in item.children" v-if="!child.hidden&&child.id==0&&level==99" :key="child.id" class="el-menu-item" style="padding-left: 40px;" :class="$route.path==child.path?'is-active':''" @click="$router.push(child.path)">
+								<li v-for="child in item.children" v-if="!child.hidden&&child.id==9&&level==99" :key="child.id" class="el-menu-item" style="padding-left: 40px;" :class="$route.path==child.path?'is-active':''" @click="$router.push(child.path)">
                   {{child.name}}
                 </li>
 							</ul>
 						</template>
 
-						<template v-if="item.leaf&&item.children.length>0">
+						<template v-if="!item.shop&&item.leaf&&item.children.length>0">
+							<li class="el-submenu">
+								<div class="el-submenu__title el-menu-item" style="padding-left: 20px;height: 56px;line-height: 56px;padding: 0 20px;" :class="$route.path==item.children[0].path?'is-active':''" @click="$router.push(item.children[0].path)"><i :class="item.iconCls"></i></div>
+							</li>
+						</template>
+
+            <template v-if="level==99&&item.shop&&item.leaf&&item.children.length>0">
 							<li class="el-submenu">
 								<div class="el-submenu__title el-menu-item" style="padding-left: 20px;height: 56px;line-height: 56px;padding: 0 20px;" :class="$route.path==item.children[0].path?'is-active':''" @click="$router.push(item.children[0].path)"><i :class="item.iconCls"></i></div>
 							</li>
